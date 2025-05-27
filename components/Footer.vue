92f347214ead34de10dba2b80dbd5c6bf86babe0
Haresh Vekriya <haresh.vekriya@gmail.com>
2025-05-27 13:34:14 +0100

    Credit footer

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="w-full py-4 text-center text-sm text-gray-600 border-t mt-8">
+      <p>
+        © {{ new Date().getFullYear() }} Developed by 
+        <a href="https://github.com/dhruvruda" target="_blank" rel="noopener" class="text-blue-600 hover:underline">Dhruv Ruda</a> &amp; 
+        <a href="https://github.com/hvekriya" target="_blank" rel="noopener" class="text-blue-600 hover:underline">Haresh Vekriya</a>
+      </p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+  }
+  </script>
+  
+  <style scoped>
+  /* Optional: you can style footer further here */
+  </style>
+  
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="min-h-screen flex flex-col">
+      <Nuxt />
+      <Footer />
+    </div>
+  </template>
+  
+  <script>
+  import Footer from '~/components/Footer.vue'
+  
+  export default {
+    components: { Footer }
+  }
+  </script>
+  
